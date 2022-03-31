873206937452e01fff78474ddb5062d2806a58bb
Alma <96235009+alma1331@users.noreply.github.com>
2022-03-31 03:20:55 +0200

    Add files via upload

---
+++ ex08/src/index.js
@@ -0,0 +1,12 @@
+function main(numOne, numTwo) {
+var numOne;
+var numTwo;
+var sum;
+var sum = numOne + numTwo;
+ 
+ return sum;
+}
+
+console.log(main(5, 8), main(5, 28), main(150, 425));
+
+module.exports = main;
