0a8e95d551acc4d9d14858a45c841db12229e6b3
Alma <96235009+alma1331@users.noreply.github.com>
2022-03-31 03:25:15 +0200

    Add files via upload

---
+++ ex09/src/index.js
@@ -0,0 +1,12 @@
+function main(numOne, numTwo) {
+ var numOne;
+ var numTwo;
+ var difference;
+ var difference = numOne - numTwo;
+  
+  return difference;
+ }
+ 
+ console.log(main(15, 8), main(25, 8), main(425, 120));
+ 
+ module.exports = main;
